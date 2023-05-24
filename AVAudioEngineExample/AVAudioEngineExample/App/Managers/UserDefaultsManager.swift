90679ac57630a4e5c63b3a8f86aced09552bdc13
doyeonjeong <debby_@kakao.com>
2023-05-24 20:06:45 +0900

    ➕[Add]: UserManager Key 추가

--- AVAudioEngineExample/AVAudioEngineExample/App/Managers/UserDefaultsManager.swift
+++ AVAudioEngineExample/AVAudioEngineExample/App/Managers/UserDefaultsManager.swift
@@ -7,9 +7,17 @@
 
 import Foundation
 
+extension UserDefaults {
+    struct Keys {
+        static let customSound = "CUSTOM_SOUND_KEY"
+    }
+}
+
 final class UserDefaultsManager {
     static let shared = UserDefaultsManager()
     private let standard = UserDefaults.standard
+    private let CUSTOM_SOUND_KEY = UserDefaults.Keys.customSound
+    private init() {}
 }
 
 // MARK: - Data Get, Set Properties
